e1fa17b7272c50d31aae5c486924a53addc11fc3
Timothy Mei <timothymei3270@gmail.com>
2022-07-22 12:25:17 -0400

    made heroku fixes

--- db/index.js
+++ db/index.js
@@ -4,7 +4,7 @@ require('dotenv').config() // Add this line
 let dbUrl =
   process.env.NODE_ENV === 'production'
     ? process.env.MONGODB_URI
-    : 'mongodb+srv://yes:Yes12345@cluster0.rnkbkky.mongodb.net/?retryWrites=true&w=majority/parkDatabase'
+    : 'mongodb://127.0.0.1:27017/parkDatabase'
 
 mongoose
   .connect(dbUrl)
