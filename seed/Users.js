180bdfcf78dd8f59f90144d0fd57ebb85bb5990c
Jackson McHugh <jackson.mchugh@gmail.com>
2022-07-22 10:21:46 -0400

    update image again

--- seed/Users.js
+++ seed/Users.js
@@ -1,33 +1,33 @@
-// const db = require('../db')
-// const { User } = require('../models')
+const db = require('../db')
+const { User } = require('../models')
 
 const { Ride } = require('../models')
 
-// db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-// const main = async () => {
-let rides = await Ride.find({})
-//   const user = [
-//     {
-//       first_name: 'John',
-//       last_name: 'Smith',
-//       age: 45,
-//       ride: ride4.idtask,
-//       show: show1.idtask
-//     },
-//     {
-//       first_name: 'John',
-//       last_name: 'Doe',
-//       age: 35,
-//       ride: ride2.idtask,
-//       show: show4.idtask
-//     }
-//   ]
-// }
+const main = async () => {
+  let rides = await Ride.find({})
+  const user = [
+    {
+      first_name: 'John',
+      last_name: 'Smith',
+      age: 45,
+      ride: ride4.idtask,
+      show: show1.idtask
+    },
+    {
+      first_name: 'John',
+      last_name: 'Doe',
+      age: 35,
+      ride: ride2.idtask,
+      show: show4.idtask
+    }
+  ]
+}
 
-// const run = async () => {
-//   await main()
-//   db.close()
-// }
+const run = async () => {
+  await main()
+  db.close()
+}
 
-// run()
+run()
